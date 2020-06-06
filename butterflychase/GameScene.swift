2f4a827b7d95af32f795557b6518fa50f48d2c4a
Vatsal Vora <vatsalajvora@gmail.com>
2020-06-06 17:52:36 -0400

    added sound, counter, distance logic

--- butterflychase/GameScene.swift
+++ butterflychase/GameScene.swift
@@ -17,32 +17,40 @@ class GameScene: SKScene {
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var butterfly : SKSpriteNode?
-    private var bottle : SKSpriteNode?
+    private var chaser : SKSpriteNode?
     private var background : SKSpriteNode?
+    private var caught : Int?
     
     override func sceneDidLoad() {
 
         self.lastUpdateTime = 0
+        self.caught = 1
         
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        self.label = SKLabelNode(text: "Butterfly")
         if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+            label.alpha = 1
+            label.zPosition = 5
+            label.position = CGPoint(x: -100,
+            y: 300)
+            let fade = SKAction.fadeIn(withDuration: 2.0)
+            label.run(fade)
+            self.addChild(label)
         }
-        
 
         self.butterfly = SKSpriteNode(imageNamed: "butterfly")
-        self.bottle = SKSpriteNode(imageNamed: "bottle")
+        self.chaser = SKSpriteNode(imageNamed: "butterfly")
         if let butterfly = self.butterfly {
             butterfly.zPosition=10
+            butterfly.position = CGPoint(x: 0,
+            y: 0)
             let colorize = SKAction.colorize(with: .white, colorBlendFactor: 1, duration: 5)
             butterfly.run(colorize)
+            self.addChild(butterfly)
         }
-        if let bottle = self.bottle {
-            bottle.zPosition=5
-            let colorize = SKAction.colorize(with: .white, colorBlendFactor: 1, duration: 5)
-            bottle.run(colorize)
+        if let chaser = self.chaser {
+            chaser.zPosition=5
+            self.addChild(chaser)
         }
     }
     
@@ -52,72 +60,57 @@ class GameScene: SKScene {
             if let background = self.background {
                 background.zPosition = 1
                 background.size =  view.frame.size
-                background.position = CGPoint(x: 0,
-                                              y: 0)
                 self.addChild(background)
             }
         }
+        let music = SKAudioNode(fileNamed: "music.caf")
+        music.isPositional = false
+        music.autoplayLooped = true
+        let play = SKAction.play()
+        music.run(play)
+        self.addChild(music)
+        
     }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let bottle = self.bottle?.copy() as! SKSpriteNode? {
-            bottle.position = pos
-            bottle.zPosition = 5
-            let colorize = SKAction.colorize(with: .green, colorBlendFactor: 1, duration: 5)
-            bottle.run(colorize)
-            self.addChild(bottle)
-        }
-        if let butterfly = self.butterfly?.copy() as! SKSpriteNode? {
-            let number = Int.random(in: 0 ..< 10)
-            butterfly.position = CGPoint(x: butterfly.position.x+CGFloat(number), y: butterfly.position.y+CGFloat(number))
-            butterfly.zPosition = 10
-            let colorize = SKAction.colorize(with: .blue, colorBlendFactor: 1, duration: 5)
-            butterfly.run(colorize)
-            self.addChild(butterfly)
-        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let bottle = self.bottle?.copy() as! SKSpriteNode? {
-            bottle.position = pos
-            bottle.zPosition = 5
-            let colorize = SKAction.colorize(with: .blue, colorBlendFactor: 1, duration: 5)
-            bottle.run(colorize)
-            self.addChild(bottle)
-        }
-        if let butterfly = self.butterfly?.copy() as! SKSpriteNode? {
-            let number = Int.random(in: 0 ..< 10)
-            butterfly.position = CGPoint(x: butterfly.position.x+CGFloat(number), y: butterfly.position.y+CGFloat(number))
-            butterfly.zPosition = 10
-            let colorize = SKAction.colorize(with: .red, colorBlendFactor: 1, duration: 5)
-            butterfly.run(colorize)
-            self.addChild(butterfly)
+        if let chaser = self.chaser {
+            let move = SKAction.move(to: pos, duration: 0.1)
+            chaser.run(move)
+            chaser.position = pos
+            if let butterfly = self.butterfly {
+                let xNumber = Int.random(in: -300 ..< 300)
+                let yNumber = Int.random(in: -300 ..< 300)
+                let xNewPos = (xNumber + Int(butterfly.position.x))%(Int(self.frame.width)/2)
+                let yNewPos = (yNumber + Int(butterfly.position.y))%(Int(self.frame.height)/2)
+                let newPos = CGPoint(x: CGFloat(xNewPos), y: CGFloat(yNewPos))
+                butterfly.position = newPos
+                let move = SKAction.move(to: newPos, duration: 0.1)
+                butterfly.run(move)
+                let dist = (butterfly.position.x - chaser.position.x) * (butterfly.position.x - chaser.position.x) + (butterfly.position.y - chaser.position.y) * (butterfly.position.y - chaser.position.y)
+                if dist < 100 {
+                    print("Caught!!")
+                    if let label = self.label {
+                        if let caught = self.caught {
+                            label.alpha = 1
+                            label.zPosition = 5
+                            let fade = SKAction.fadeIn(withDuration: 1)
+                            label.text = "Caught: " + String(caught)
+                            self.caught = caught + 1
+                            label.run(fade)
+                        }
+                    }
+                }
+            }
         }
     }
-    
+        
     func touchUp(atPoint pos : CGPoint) {
-        if let bottle = self.bottle?.copy() as! SKSpriteNode? {
-            bottle.position = pos
-            bottle.zPosition = 5
-            let colorize = SKAction.colorize(with: .red, colorBlendFactor: 1, duration: 5)
-            bottle.run(colorize)
-            self.addChild(bottle)
-        }
-        if let butterfly = self.butterfly?.copy() as! SKSpriteNode? {
-            let number = Int.random(in: 0 ..< 10)
-            butterfly.position = CGPoint(x: butterfly.position.x+CGFloat(number), y: butterfly.position.y+CGFloat(number))
-            butterfly.zPosition = 10
-            let colorize = SKAction.colorize(with: .green, colorBlendFactor: 1, duration: 5)
-            butterfly.run(colorize)
-            self.addChild(butterfly)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
